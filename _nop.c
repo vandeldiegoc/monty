ad56923f84d7a9e5c54ce633400ed618232b8dde
Diego <vandeldiegoc@gmail.com>
2020-05-14 23:49:18 -0500

    nop

--- _nop.c
+++ _nop.c
@@ -1,10 +1,10 @@
 #include "monty.h"
 /**
- * nop - make nothing
+ * _nop - make nothing
  * @stack:  stack of nodo
  * @l: line number
  */
-void nop(stack_t **stack, unsigned int l)
+void _nop(stack_t **stack, unsigned int l)
 {
 	(void)stack;
 	(void)l;
