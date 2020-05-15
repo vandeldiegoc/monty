ae3d446d0e0387f3b7c789e66313dadf5b60884c
Diego <vandeldiegoc@gmail.com>
2020-05-14 23:43:08 -0500

    nop

--- _nop.c
+++ _nop.c
@@ -4,7 +4,7 @@
  * @stack:  stack of nodo
  * @line_number: line number
  */
-void nop(stack_t **stack, unsigned int line_number)
+void nop(stack_t **stack, unsigned int l)
 {
 	(void)stack;
 	(void)line_number;
