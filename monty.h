13512c07768a585d854dd0e1b0ac6171023a701e
Diego <vandeldiegoc@gmail.com>
2020-05-15 00:00:20 -0500

    4

--- monty.h
+++ monty.h
@@ -40,4 +40,5 @@ void push_funtion(char *number, stack_t **stack, unsigned int numline);
 void get_funtion(char *string, stack_t **stack, unsigned int numline);
 int str_is_num(char *str);
 void free_stack(stack_t **head);
+
 #endif
