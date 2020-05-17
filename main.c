69f9f0bf1505031ba26894fb500656832124d0c3
Diego <vandeldiegoc@gmail.com>
2020-05-17 18:06:51 -0500

    blank line

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ unsigned int numline = 0;
 char *line = NULL, *tokens = NULL;
 FILE *opfile;
 size_t len;
+char *delimit = " \t\r\n";
 if (argc != 2)
 {
 	printf("USAGE: monty file\n");
@@ -25,24 +26,22 @@ if (opfile == NULL)
 }
 while (getline(&line, &len, opfile) != -1)
 {
-	tokens = strtok(line, "\n \t");
 	numline++;
-	if (tokens[0] == '#')
+	tokens = strtok(line, delimit);
+	if (tokens == NULL || tokens[0] == '#')
 		continue;
 
 	if (strcmp(tokens, "push") == 0)
 	{
-		tokens = strtok(NULL, "\n \t");
+	tokens = strtok(NULL, delimit);
 		push_funtion(tokens, &stack, numline);
 	}
 	else
-	{
 		get_funtion(tokens, &stack, numline);
-	}
+	tokens = strtok(NULL, delimit);
 }
-
 free_stack(&stack);
 fclose(opfile);
 free(line);
-return (0);
+exit(EXIT_FAILURE);
 }
