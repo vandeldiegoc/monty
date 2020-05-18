0e1f272ceb6627d3d6db64ddde7edf1f26f7e3cf
Diego <vandeldiegoc@gmail.com>
2020-05-17 20:43:55 -0500

    0v2

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ if (argc != 2)
 opfile = fopen(argv[1], "r");
 if (opfile == NULL)
 {
-	fprintf(stderr, "Error: Can't open file %s", argv[1]);
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 	exit(EXIT_FAILURE);
 }
 while (getline(&line, &len, opfile) != -1)
